e00272ad63f1ee82cd367a737583d127f7deb674
mishangoti <mishan2512@gmail.com>
2023-08-02 13:39:59 -0400

    crud rest api with apache derby runtime sql database

---
+++ src/test/java/com/springboot/courseapi/CourseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
